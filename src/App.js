52b087c59b00fc8ec936cb14514de64e8fd1729c
richardcsedge <132105798+richardcsedge@users.noreply.github.com>
2023-05-19 17:17:50 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrementClick = () => {
+    setCount(count + 1);
+  };
+  const onDecrementClick = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="container">
       <h1>code-next-react-counter</h1>
@@ -10,7 +19,18 @@ export const App = () => {
       <hr />
 
       <div>
-        {/* YOUR COUNTER CODE HERE */}
+        <h2>count : {count}</h2>
+
+        <button
+          className="btn btn-success"
+          onClick={onIncrementClick}
+          style={{marginRight: '9px' }}
+        >
+          increment
+        </button>
+        <button className="btn btn-warning" onClick={onDecrementClick}>
+          decrement
+        </button>
       </div>
     </div>
   );
